2b4b9a2bfaf730e3a3d425873c2edbf5467b78ba
Thiago S. Marcelino <109177328+ThiagoDSMarcelino@users.noreply.github.com>
2023-08-04 22:23:40 +0000

    LinkedList push method implemented

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,14 +1,14 @@
 #[derive(Debug, Clone, Default)]
 pub struct LinkedList<T> {
     value: Option<T>,
-    next: Option<Box<LinkedList<T>>>
+    next: Option<Box<LinkedList<T>>>,
 }
 
-impl<T> LinkedList<T> {
-    pub fn new(value: T) -> LinkedList<T> {
+impl<T: Copy> LinkedList<T> {
+    pub fn new(value: &T) -> LinkedList<T> {
         LinkedList {
-            value: Some(value),
-            next: None
+            value: Some(*value),
+            next: None,
         }
     }
 
@@ -25,5 +25,16 @@ impl<T> LinkedList<T> {
             Some(node) => node.iterate_get(count + 1, index),
             None => &None,
         }
+    }
+
+    pub fn push(&mut self, value: &T) {
+        if self.value.is_none() {
+            self.value = Some(*value);
+        };
+
+        match self.next.as_mut() {
+            Some(node) => node.push(value),
+            None => self.next = Some(Box::new(LinkedList::new(value))),
+        };
     }
 }
--- tests/linked_list_test.rs
+++ tests/linked_list_test.rs
@@ -4,9 +4,17 @@ mod tests {
 
     #[test]
     fn test_get() {
-        let mut list: LinkedList<i32> = LinkedList::<i32>::new(2);
-
+        let mut list: LinkedList<i32> = LinkedList::new(&2);
         assert_eq!(list.get(0), &Some(2));
         assert_eq!(list.get(1), &None);
+    }
+
+    #[test]
+    fn test_push() {
+        let mut list: LinkedList<i32> = LinkedList::new(&2);
+        list.push(&3);
+
+        assert_eq!(list.get(0), &Some(2));
+        assert_eq!(list.get(1), &Some(3));
     }
 }
