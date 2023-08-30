cf40d2f5cc5dfeb2e78e4f8e865ad1b614653a70
Thiago S. Marcelino <109177328+ThiagoDSMarcelino@users.noreply.github.com>
2023-08-30 22:54:20 +0000

    Pop methods and Iterator trait were implemented

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,4 +1,4 @@
-use std::ptr::NonNull;
+use std::{marker::PhantomData, ptr::NonNull};
 
 #[derive(Debug, Clone, Default)]
 pub struct LinkedList<T> {
@@ -15,12 +15,16 @@ struct Node<T> {
 }
 
 impl<T> Node<T> {
-    pub const fn new(element: T) -> Self {
+    const fn new(element: T) -> Self {
         Self {
             element,
             next: None,
             prev: None,
         }
+    }
+
+    fn into_element(self) -> T {
+        self.element
     }
 }
 
@@ -39,6 +43,20 @@ impl<T> LinkedList<T> {
 
     pub fn is_empty(&self) -> bool {
         self.head.is_none()
+    }
+
+    pub fn clear(&mut self) {
+        self.head = None;
+        self.tail = None;
+        self.length = 0;
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        self.head.map(|node| unsafe { &(*node.as_ptr()).element })
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        self.tail.map(|node| unsafe { &(*node.as_ptr()).element })
     }
 
     pub fn push_front(&mut self, value: T) {
@@ -81,11 +99,67 @@ impl<T> LinkedList<T> {
         self.length += 1;
     }
 
-    pub fn front(&self) -> Option<&T> {
-        unsafe { self.head.map(|node| &(*node.as_ptr()).element) }
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.map(|node| unsafe {
+            let node = Box::from_raw(node.as_ptr());
+
+            self.head = node.next;
+
+            match self.head {
+                Some(head) => (*head.as_ptr()).prev = None,
+                None => self.tail = None,
+            }
+
+            self.length -= 1;
+            node.into_element()
+        })
     }
 
-    pub fn back(&self) -> Option<&T> {
-        unsafe { self.tail.map(|node| &(*node.as_ptr()).element) }
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.tail.map(|node| unsafe {
+            let node = Box::from_raw(node.as_ptr());
+
+            self.tail = node.prev;
+
+            match self.tail {
+                Some(tail) => (*tail.as_ptr()).next = None,
+                None => self.head = None,
+            }
+
+            self.length -= 1;
+            node.into_element()
+        })
+    }
+}
+
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type Item = &'a T;
+    type IntoIter = NodeIterator<'a, T>;
+
+    fn into_iter(self) -> NodeIterator<'a, T> {
+        NodeIterator {
+            node: self.head,
+            index: 0,
+            marker: PhantomData,
+        }
+    }
+}
+
+pub struct NodeIterator<'a, T> {
+    node: Option<NonNull<Node<T>>>,
+    index: usize,
+    marker: PhantomData<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for NodeIterator<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<&'a T> {
+        self.node.map(|crr| unsafe {
+            self.node = (*crr.as_ptr()).next;
+            self.index += 1;
+
+            &(*crr.as_ptr()).element
+        })
     }
 }
--- tests/linked_list_test.rs
+++ tests/linked_list_test.rs
@@ -3,8 +3,16 @@ mod tests {
     use data_structures::LinkedList;
 
     #[test]
-    fn new_list() {
+    fn new() {
         let list: LinkedList<u32> = LinkedList::new();
+
+        assert!(list.is_empty());
+        assert_eq!(list.len(), 0);
+    }
+
+    #[test]
+    fn default() {
+        let list: LinkedList<u32> = LinkedList::default();
 
         assert!(list.is_empty());
         assert_eq!(list.len(), 0);
@@ -16,10 +24,11 @@ mod tests {
 
         list.push_front(5);
         list.push_front(7);
+        list.push_front(35);
 
-        assert_eq!(list.front(), Some(&7));
+        assert_eq!(list.front(), Some(&35));
         assert_eq!(list.back(), Some(&5));
-        assert_eq!(list.len(), 2);
+        assert_eq!(list.len(), 3);
     }
 
     #[test]
@@ -33,5 +42,54 @@ mod tests {
         assert_eq!(list.front(), Some(&9));
         assert_eq!(list.back(), Some(&4));
         assert_eq!(list.len(), 3);
+    }
+
+    #[test]
+    fn pop_front() {
+        let mut list: LinkedList<u32> = LinkedList::new();
+
+        list.push_front(1);
+        list.push_front(6);
+        list.push_front(12);
+
+        list.pop_front();
+
+        assert_eq!(list.front(), Some(&6));
+        assert_eq!(list.back(), Some(&1));
+        assert_eq!(list.len(), 2);
+    }
+
+    #[test]
+    fn pop_back() {
+        let mut list: LinkedList<u32> = LinkedList::new();
+
+        list.push_back(18);
+        list.push_back(56);
+        list.push_back(33);
+
+        list.pop_back();
+
+        assert_eq!(list.front(), Some(&18));
+        assert_eq!(list.back(), Some(&56));
+        assert_eq!(list.len(), 2);
+    }
+
+    #[test]
+    fn iter() {
+        let vec = [3, 5, 8, 4];
+        let mut list: LinkedList<u32> = LinkedList::new();
+
+        for i in vec {
+            list.push_back(i);
+        }
+
+        let mut i: usize = 0;
+
+        let iter = list.into_iter();
+
+        for elem in iter {
+            assert_eq!(&vec[i], elem);
+            i += 1;
+        }
     }
 }
