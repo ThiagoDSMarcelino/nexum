77494052f6d93cabb036ad5cab39b653d10ed3fe
ThiagoDSMarcelino <thiagodsmarcelino@gmail.com>
2023-11-04 21:14:08 -0300

    Basic tests created

--- tests/auto_expand_vector_test.rs
+++ tests/auto_expand_vector_test.rs
@@ -4,7 +4,26 @@ mod tests {
 
     #[test]
     fn new() {
-        let hash_table: AutoExpandVector<i32> = AutoExpandVector::new();
-        assert_eq!(hash_table.capacity(), 0);
+        let vec: AutoExpandVector<i32> = AutoExpandVector::new();
+        assert_eq!(vec.capacity(), 0);
+    }
+
+    #[test]
+    fn push() {
+        let mut vec = AutoExpandVector::new();
+        vec.push(1);
+        assert_eq!(vec.capacity(), 1);
+        assert_eq!(vec.len(), 1);
+        assert_eq!(vec.get(0), &Some(1));
+    }
+
+    #[test]
+    fn clear() {
+        let mut vec = AutoExpandVector::new();
+        vec.push(1);
+        vec.clear();
+        assert_eq!(vec.capacity(), 0);
+        assert_eq!(vec.len(), 0);
+        assert_eq!(vec.get(0), &None);
     }
 }
